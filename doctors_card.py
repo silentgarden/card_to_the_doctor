783e9dc299da546ef2e5f608d8b0e1b2338d2bf2
Silent Garden <silentgarden837@gmail.com>
2023-08-05 21:21:39 +0530

    added a main function

--- doctors_card.py
+++ doctors_card.py
@@ -2,43 +2,73 @@ from tkinter import *
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
-root = Tk()
-root.title("Doctor's Card")
-root.geometry("600x500+300+100")
+def main():
+    root = Tk()
+    root.title("Doctor's Card")
+    root.geometry("600x500+300+100")
+    
+    label_main = ttk.Label(root, text="Doctor's Card", bootstyle=SUCCESS, font="Helvetica 20 bold").grid(row=0, column=0, sticky=W, padx=10)
+    label1 = ttk.Label(root, text="Name", font="Helvetica 15", bootstyle=DARK).grid(row=1, column=0, sticky=W, pady=10, padx=10) 
+    label2 = ttk.Label(root, text="Age", font="Helvetica 15", bootstyle=DARK).grid(row=2, column=0, sticky=W, pady=10, padx=10) 
+    label3 = ttk.Label(root, text="Birthday", font="Helvetica 15", bootstyle=DARK).grid(row=3, column=0, sticky=W, pady=10, padx=10) 
+    label4 = ttk.Label(root, text="Package", font="Helvetica 15", bootstyle=DARK).grid(row=4, column=0, sticky=W, pady=10, padx=10) 
+    label5 = ttk.Label(root, text="Arrival", font="Helvetica 15", bootstyle=DARK).grid(row=5, column=0, sticky=W, pady=10, padx=10) 
+    label6 = ttk.Label(root, text="Departure", font="Helvetica 15", bootstyle=DARK).grid(row=7, column=0, sticky=W, pady=10, padx=10) 
+    
+    label20 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=1, column=1, sticky=W, pady=10, padx=10)
+    label21 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=2, column=1, sticky=W, pady=10, padx=10)
+    label22 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=3, column=1, sticky=W, pady=10, padx=10)
+    label23 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=4, column=1, sticky=W, pady=10, padx=10)
+    label24 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=5, column=1, sticky=W, pady=10, padx=10)
+    label25 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=7, column=1, sticky=W, pady=10, padx=10)
+    
+    input_name = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=30) #, columnspan=5)
+    input_name.grid(row=1, column=2, columnspan=3, sticky=W, pady=10, padx=10)
+    input_age = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=6)
+    input_age.grid(row=2, column=2, sticky=W, pady=10, padx=10)
+    label_age = ttk.Label(root, text="years", font="Arial 12", bootstyle=DARK).grid(row=2, column=3, sticky=W, pady=10, padx=10)
+    input_birthday = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=11)
+    input_birthday.grid(row=3, column=2, columnspan=3, sticky=W, padx=10, pady=10)
+    
+    options = StringVar()
+    options.set("Select Package")
+    
+    package_values = ["Ayurveda Regeneration Package", "Ayurveda Panchakarma Package", "Ayurveda Slimming Package"]
+    input_package = ttk.Combobox(root, values=package_values, width=28, state='readonly', font='Arial 12')
+    input_package.grid(row=4, column=2, columnspan=3, sticky=W, pady=8, padx=8)
+    input_package.current(0)
+    
+    input_arrival = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=11)
+    input_arrival.grid(row=5, column=2, columnspan=3, sticky=W, padx=10, pady=10)
+    input_arrival.insert(END, "YYYY-MM-DD")
+    
+    input_Departure = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=11)
+    input_Departure.grid(row=7, column=2, columnspan=3, sticky=W, padx=10, pady=10)
+    input_Departure.insert(END, "YYYY-MM-DD")
+    
+    var1 = IntVar()
+    checkbox_notimespecified1 = ttk.Checkbutton(root, text="Time Not Specified", variable=var1)
+    checkbox_notimespecified1.grid(row=6, column=2, columnspan=3, sticky=W, padx=10, pady=10)
+    
+    input_time1 = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=7)
+    input_time1.grid(row=6, column=4, sticky=W, padx=10, pady=10)
+    input_time1.insert(END, 'Time')
+    
+    var2 = IntVar()
+    checkbox_notimespecified2 = ttk.Checkbutton(root, text="Time Not Specified", variable=var2)
+    checkbox_notimespecified2.grid(row=8, column=2, columnspan=3, sticky=W, padx=10, pady=10)
+    
+    input_time2 = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=7)
+    input_time2.grid(row=8, column=4, sticky=W, padx=10, pady=10)
+    input_time2.insert(END, 'Time')
+    
+    button_quit = ttk.Button(root, text='Quit', style='info.outline.TButton')
+    button_quit.grid(row=9, column=3, sticky=W, padx=10, pady=10)
+    
+    button_create = ttk.Button(root, text='Create', style='success.TButton')
+    button_create.grid(row=9, column=4, sticky=W, padx=10, pady=10)
+    
+    root.mainloop()
 
-label_main = ttk.Label(root, text="Doctor's Card", bootstyle=SUCCESS, font="Helvetica 20 bold").grid(row=0, column=0, sticky=W, padx=10)
-label1 = ttk.Label(root, text="Name", font="Helvetica 15", bootstyle=DARK).grid(row=1, column=0, sticky=W, pady=10, padx=10) 
-label2 = ttk.Label(root, text="Age", font="Helvetica 15", bootstyle=DARK).grid(row=2, column=0, sticky=W, pady=10, padx=10) 
-label3 = ttk.Label(root, text="Birthday", font="Helvetica 15", bootstyle=DARK).grid(row=3, column=0, sticky=W, pady=10, padx=10) 
-label4 = ttk.Label(root, text="Package", font="Helvetica 15", bootstyle=DARK).grid(row=4, column=0, sticky=W, pady=10, padx=10) 
-label5 = ttk.Label(root, text="Arrival", font="Helvetica 15", bootstyle=DARK).grid(row=5, column=0, sticky=W, pady=10, padx=10) 
+main()
 
-label20 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=1, column=1, sticky=W, pady=10, padx=10)
-label21 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=2, column=1, sticky=W, pady=10, padx=10)
-label22 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=3, column=1, sticky=W, pady=10, padx=10)
-label23 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=4, column=1, sticky=W, pady=10, padx=10)
-label24 = ttk.Label(root, text=":", font="Helvetica 15", bootstyle=DARK).grid(row=5, column=1, sticky=W, pady=10, padx=10)
-
-input_name = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=30) #, columnspan=5)
-input_name.grid(row=1, column=2, sticky=W, pady=10, padx=10)
-input_age = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=6)
-input_age.grid(row=2, column=2, sticky=W, pady=10, padx=10)
-label_age = ttk.Label(root, text="years", font="Arial 12", bootstyle=DARK).grid(row=2, column=3, sticky=W, pady=10, padx=10)
-input_birthday = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=11)
-input_birthday.grid(row=3, column=2, sticky=W, padx=10, pady=10)
-
-options = ttk.StringVar()
-options.set("Select Package")
-
-input_package = ttk.OptionMenu(root, options, "Ayurveda Regeneration Package", "Ayurveda Panchakarma Package", "Ayurveda Slimming Package")
-input_package.grid(row=4, column=2, sticky=W, pady=8, padx=8)
-
-input_arrival = ttk.Entry(root, font="Arial 12", bootstyle=DARK, width=11)
-input_arrival.grid(row=5, column=2, sticky=W, padx=10, pady=10)
-input_arrival.insert(END, "YYYY-MM-DD")
-
-var1 = IntVar()
-checkbox_notimespecified = ttk.Checkbutton(root, text="Time Not Specified", variable=var1)
-checkbox_notimespecified.grid(row=6, column=2, sticky=W, padx=10, pady=10)
-
-root.mainloop()
